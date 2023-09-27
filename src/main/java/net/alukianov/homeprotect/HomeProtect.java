1f487d0215a7bdfef867b58c09c25f59aaa412be
alukianov <101193473+aLukianov63@users.noreply.github.com>
2023-09-27 22:30:32 +0400

    🐞 code fix

--- src/main/java/net/alukianov/homeprotect/HomeProtect.java
+++ src/main/java/net/alukianov/homeprotect/HomeProtect.java
@@ -1,27 +1,4 @@
 
-/*
- * This file is part of HomeProtect, licensed under the MIT License.
- *
- * Copyright (C) 2022 by ZxcReaper
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without l> imitation the rights to use, copy, modify, merge,
- * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to
- * whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package net.alukianov.homeprotect;
 
 import co.aikar.commands.MessageKeys;
@@ -35,10 +12,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HomeProtect extends JavaPlugin {
-
-    /*
-    Sound.ENTITY_ITEM_BREAK <ERROR>
-     */
 
     private AreaManager areaManager = new AreaManager();
 
